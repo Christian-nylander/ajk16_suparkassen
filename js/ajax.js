a377b2b4e753c8b96ba3cb1b09d45deb34eaa81a
Stefan Bahnson <stefan.bahnson@findwise.com>
2017-01-29 13:57:15 +0100

    change url

--- js/ajax.js
+++ js/ajax.js
@@ -1,8 +1,8 @@
-var BASE_URL = 'https://suparkassen-api.herokuapp.com/api/products';
+var BASE_URL = 'https://suparkassen-api.herokuapp.com/api';
 
 function getProducts() {
   $.ajax({
-    url: BASE_URL,
+    url: BASE_URL + '/products',
     type: 'GET',
     success: renderProducts,
     error: function (err) {
@@ -34,7 +34,7 @@ function sendData(event) {
     // console.log(data);
     $.ajax({
         type: "POST",
-        url: 'http://localhost:3000/api/payment',
+        url: BASE_URL + '/payment',
         contentType: "application/json",
         data: JSON.stringify(data),
         success: function (data) {
